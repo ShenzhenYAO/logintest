eb8935026cf75067b4c98d800585a7d50eb79b98
Shenzhen YAO <junk.them@hotmail.com>
2020-09-21 14:12:21 -0600

    v4

--- input/data.js
+++ input/data.js
@@ -1,20 +1,6 @@
 var signupform, thestage, signup;
 var auth=undefined;
 
-//firebase database settings
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyBSK9U03kHwBBB3bgNh0Jo9tEVY7y4A16I",
-    authDomain: "logintest-bb124.firebaseapp.com",
-    databaseURL: "https://logintest-bb124.firebaseio.com",
-    projectId: "logintest-bb124",
-    storageBucket: "logintest-bb124.appspot.com",
-    messagingSenderId: "1084200182001",
-    appId: "1:1084200182001:web:ffaaa2c5adb9e9d8e34950",
-    measurementId: "G-3X94PC3T8K"
-};
-
 // data for creating the nav bar and its offstring nodes
 var navdomsdata = {
     parent: document.body, // parent is an html dom element like <body></body>
